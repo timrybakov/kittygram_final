e2f9c6249bc9762440ffeeb74d9b6bfeaf8903a6
Тимур Рыбаков <timurjh0608@icloud.come>
2024-02-17 16:01:39 +0100

    ejbfiefbei

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -62,8 +62,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
